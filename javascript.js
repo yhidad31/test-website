e040e94c2ef4f40f3028545318eb1b57b87f2ac1
S. Marie <sdhmariedesign@gmail.com>
2019-07-24 08:12:10 -0700

    First commit

---
+++ javascript.js
@@ -0,0 +1,24 @@
+//STICKY NAVBAR
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//NAVBAR HAMBURGER
+function navbarHamburger() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
